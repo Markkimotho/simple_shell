065a7d50878a374bd93ddd235a3323eab2bd58a6
KateShelyn <kateshelyn@gmail.com>
2022-11-08 02:30:09 -0800

    haha still typing with on-screen keyboard

---
+++ shell.h
@@ -0,0 +1,17 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdarg.h>
+#include <assert.h>
+#include <errno.h>
+
+/*
+ * Th main header file
+ * remaining to identify the operators in a normal shell interface
+ */
+
+
